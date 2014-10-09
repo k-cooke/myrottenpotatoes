7798744305613eac31bacb4eabecf037b1509174
K. Cooke <kcooke@uncc.edu>
2014-10-09 03:03:46 -0300

    Start of Sec 4_8 - top page 129

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,4 +12,15 @@ class MoviesController < ApplicationController
   @movie = Movie.create!(params[:movie])
   redirect_to movies_path
   end
+
+  def edit
+  @movie = Movie.find params[:id]
+  end
+ 
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
 end
